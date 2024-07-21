5bb1c6f6fc0f0855043c0aaa635c66f1dcf17174
Ripandy Adha <ripandy.adha@gmail.com>
2024-07-22 05:26:18 +0900

    refactor: [GameEvent] remove UniRx/UniTask integration

--- Runtime/External/R3/GameEvent.R3.cs
+++ Runtime/External/R3/GameEvent.R3.cs
@@ -9,79 +9,18 @@ namespace Kadinche.Kassets.EventSystem
 {
     public partial class GameEvent
     {
-        private readonly Subject<object> _subject = new Subject<object>();
+        private readonly Subject<object> _subject = new();
     }
 
     public abstract partial class GameEvent<T>
     {
-        private readonly Subject<T> _valueSubject = new Subject<T>();
+        private readonly Subject<T> _valueSubject = new();
         
         public Observable<T> AsObservable() => _valueSubject;
         public IObservable<T> AsSystemObservable() => _valueSubject.AsSystemObservable();
         public IAsyncEnumerable<T> ToAsyncEnumerable(CancellationToken cancellationToken = default) => _valueSubject.ToAsyncEnumerable(cancellationToken: cancellationToken);
     }
     
-#if KASSETS_UNIRX
-    public partial class GameEvent : IObserver<object>, IObservable<object>
-    {
-        void IObserver<object>.OnCompleted() => _subject.OnCompleted();
-        void IObserver<object>.OnError(Exception error) => _subject.OnCompleted(error);
-        void IObserver<object>.OnNext(object value) => Raise();
-        IDisposable IObservable<object>.Subscribe(IObserver<object> observer) => _subject.AsSystemObservable().Subscribe(observer);
-    }
-
-    public abstract partial class GameEvent<T> : IObserver<T>, IObservable<T>
-    {
-        void IObserver<T>.OnCompleted() => _valueSubject.OnCompleted();
-        void IObserver<T>.OnError(Exception error) => _valueSubject.OnCompleted(error);
-        void IObserver<T>.OnNext(T value) => Raise(value);
-        IDisposable IObservable<T>.Subscribe(IObserver<T> observer) => AsSystemObservable().Subscribe(observer);
-    }
-#endif
-    
-#if KASSETS_UNITASK
-    public partial class GameEvent
-    {
-        protected void Raise_R3() => _subject.OnNext(this);
-        private IDisposable Subscribe_R3(Action action) => _subject.Subscribe(_ => action.Invoke());
-        protected virtual void Dispose_R3() => _subject.Dispose();
-    }
-    
-    public abstract partial class GameEvent<T>
-    {
-        /// <summary>
-        /// Raise the event with parameter.
-        /// </summary>
-        /// /// <param name="param"></param>
-        private void Raise_R3(T param)
-        {
-            _value = param;
-            base.Raise_R3();
-            _valueSubject.OnNext(param);
-        }
-
-        private IDisposable Subscribe_R3(Action<T> action) => _valueSubject.Subscribe(action);
-        
-        protected override void Dispose_R3()
-        {
-            _valueSubject.Dispose();
-            base.Dispose_R3();
-        }
-    }
-    
-    public partial class GameEvent
-    {
-        private void Raise_UniRx() => Raise_R3();
-        private IDisposable Subscribe_UniRx(Action action) => Subscribe_R3(action);
-        private void Dispose_UniRx() => Dispose_R3();
-    }
-
-    public abstract partial class GameEvent<T>
-    {
-        private void Raise_UniRx(T param) => Raise_R3(param);
-        private IDisposable Subscribe_UniRx(Action<T> action) => Subscribe_R3(action);
-    }
-#else
     public partial class GameEvent
     {
         /// <summary>
@@ -117,7 +56,6 @@ namespace Kadinche.Kassets.EventSystem
             base.Dispose();
         }
     }
-#endif
 }
 
 #endif
