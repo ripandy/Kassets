57cce79d88d5edd23c392c4b001b7f4e131f3a12
Ripandy Adha <ripandy.adha@gmail.com>
2024-07-22 11:14:18 +0900

    feat: add EventAsync to GameEvent.R3

--- Runtime/External/R3/GameEvent.R3.cs
+++ Runtime/External/R3/GameEvent.R3.cs
@@ -3,13 +3,43 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using R3;
 
 namespace Kadinche.Kassets.EventSystem
 {
     public partial class GameEvent
     {
+        private CancellationTokenSource cts;
+        protected CancellationToken DefaultToken
+        {
+            get
+            {
+                cts ??= new CancellationTokenSource();
+                return cts.Token;
+            }
+        }
+        
         private readonly Subject<object> _subject = new();
+        
+        /// <summary>
+        /// Raise the event.
+        /// </summary>
+        public virtual void Raise() => _subject.OnNext(this);
+        public IDisposable Subscribe(Action action) => _subject.Subscribe(_ => action.Invoke());
+        
+        public async ValueTask EventAsync(CancellationToken cancellationToken = default)
+        {
+            var linkedTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, DefaultToken);
+            await _subject.WaitAsync(cancellationToken: linkedTokenSource.Token);
+        }
+
+        public override void Dispose()
+        {
+            _subject.Dispose();
+            cts?.CancelAndDispose();
+            cts = null;
+        }
     }
 
     public abstract partial class GameEvent<T>
@@ -19,24 +49,7 @@ namespace Kadinche.Kassets.EventSystem
         public Observable<T> AsObservable() => _valueSubject;
         public IObservable<T> AsSystemObservable() => _valueSubject.AsSystemObservable();
         public IAsyncEnumerable<T> ToAsyncEnumerable(CancellationToken cancellationToken = default) => _valueSubject.ToAsyncEnumerable(cancellationToken: cancellationToken);
-    }
     
-    public partial class GameEvent
-    {
-        /// <summary>
-        /// Raise the event.
-        /// </summary>
-        public virtual void Raise() => _subject.OnNext(this);
-        public IDisposable Subscribe(Action action) => _subject.Subscribe(_ => action.Invoke());
-        public override void Dispose()
-        {
-            _subject.Dispose();
-            base.Dispose();
-        }
-    }
-    
-    public abstract partial class GameEvent<T>
-    {
         /// <summary>
         /// Raise the event with parameter.
         /// </summary>
@@ -49,6 +62,12 @@ namespace Kadinche.Kassets.EventSystem
         }
 
         public IDisposable Subscribe(Action<T> action) => _valueSubject.Subscribe(action);
+        
+        public new async ValueTask<T> EventAsync(CancellationToken cancellationToken = default)
+        {
+            var linkedTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, DefaultToken);
+            return await _valueSubject.LastOrDefaultAsync(cancellationToken: linkedTokenSource.Token);
+        }
         
         public override void Dispose()
         {
