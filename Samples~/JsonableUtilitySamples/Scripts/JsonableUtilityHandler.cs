b21087dfb3986e2aab7f1d63aa0f175ebfccce0a
Ripandy Adha <ripandy.adha@gmail.com>
2024-07-30 17:52:52 +0900

    refactor: update Jsonable utility sample

---
+++ Samples~/JsonableUtilitySamples/Scripts/JsonableUtilityHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kadinche.Kassets.Variable.Sample
+{
+    public class JsonableUtilityHandler : MonoBehaviour
+    {
+        [SerializeField] private CustomVariable customVariable;
+        [SerializeField] private Button saveButton;
+        [SerializeField] private Button loadButton;
+
+        private void OnEnable()
+        {
+            saveButton.onClick.AddListener(SaveButtonPressed);
+            loadButton.onClick.AddListener(LoadButtonPressed);
+        }
+        
+        private void OnDisable()
+        {
+            saveButton.onClick.RemoveListener(SaveButtonPressed);
+            loadButton.onClick.RemoveListener(LoadButtonPressed);
+        }
+
+        private void SaveButtonPressed()
+        {
+            customVariable.SaveToJson();
+            
+            // uncomment to try the experimental non generic handling.
+            // IJsonable jsonable = customVariable;
+            // jsonable.SaveToJson();
+            
+            Debug.Log($"[{GetType().Name}] {nameof(customVariable)} saved as json named {customVariable.name}.json");
+        }
+        
+        private void LoadButtonPressed()
+        {
+            customVariable.LoadFromJson();
+            
+            // uncomment to try the experimental non generic handling.
+            // IJsonable jsonable = customVariable;
+            // jsonable.LoadFromJson();
+            
+            Debug.Log($"[{GetType().Name}] loaded {customVariable.name}.json into {nameof(customVariable)}");
+        }
+    }
+}
