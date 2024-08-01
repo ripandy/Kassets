fd75b5ffe380cd88c53c59eb55a7b84f7e22699a
Ripandy Adha <ripandy.adha@gmail.com>
2024-08-01 20:08:25 +0900

    chore: remove scripting define, since min version has been updated

--- Runtime/Base/ObjectPools/GameObjectPool.cs
+++ Runtime/Base/ObjectPools/GameObjectPool.cs
@@ -1,5 +1,3 @@
-#if UNITY_2021_1_OR_NEWER
-
 using UnityEngine;
 
 namespace Kadinche.Kassets.ObjectPool
@@ -22,5 +20,3 @@ namespace Kadinche.Kassets.ObjectPool
         protected override void OnDestroyPoolObject(GameObject go) => Destroy(go);
     }
 }
-
-#endif
